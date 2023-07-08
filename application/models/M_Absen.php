9b6a848ee3d587f995bb3bf1abcf4af9d0ad02ac
Fajar Alif Riyandi <fajariyandi36@gmail.com>
2023-07-09 01:10:24 +0700

    Update M_Absen.php

--- application/models/M_Absen.php
+++ application/models/M_Absen.php
@@ -26,14 +26,36 @@ class M_Absen extends CI_Model
 		$this->db->from('tb_present n');
 		$this->db->join('tb_student s', 's.student_id = n.student_id');
 		$query = $this->db->get_where('tb_present',['n.assessment_id'=> $id,'n.present_date' => $date]);
+//		$this->db->where('n.present_date', $date);
+//        $query = $this->db->get_where('tb_present',['assessment_id'=>$id]);
         return $query->result_array();
     }
 
     public function GetByIdAbsen($id,$date)
     {
+//		$this->db->select('n.present_id, `n.assessment_id`, `n.student_id`,`n.present_date`, `n.present_status`,
+//		`n.present_ket`, s.fullname, s.address');
+//		$this->db->from('tb_present n');
+//		$this->db->join('tb_student s', 's.student_id = n.student_id');
+//		$this->db->where('n.assessment_id', $id);
+//		$this->db->where('n.present_date', $date);
+//		$query = $this->db->get('tb_present');
+////		$result = $query->result_array();s
+//
+////        $query = $this->db->get_where($this->user_tabel, [date('present_date') => $id]);
+//        return $query->result_array();
+//		$this->db->select('n.present_id, `n.assessment_id`, `n.student_id`,`n.present_date`, `n.present_status`,
+//		`n.present_ket`, s.fullname, s.address');
+//		$this->db->from('tb_present n');
+//		$this->db->join('tb_student s', 's.student_id = n.student_id');
+//		$query = $this->db->get_where('tb_present',['n.assessment_id'=> $id,'n.present_date' => $date]);
+////		$this->db->where('n.present_date', $date);
+////        $query = $this->db->get_where('tb_present',['assessment_id'=>$id]);
+//		return $query->result_array();
 		$sql = "SELECT n.present_id, n.assessment_id, n.student_id,n.present_date, n.present_status,
-		n.present_ket, s.fullname, s.address FROM tb_present n INNER JOIN tb_student s ON s.student_id = n.student_id 
+		n.present_ket, n.present_date_updated, s.fullname, s.address FROM tb_present n INNER JOIN tb_student s ON s.student_id = n.student_id 
 		WHERE n.assessment_id = '$id' AND n.present_date='$date'";
+//		$this->db->or_like('assessment_id',  $id);
 		$query = $this->db->query($sql)->result_array();
 		return $query;
     }
