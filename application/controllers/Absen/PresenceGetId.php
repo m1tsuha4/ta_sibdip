c073838bbfa64e9992f068ee862facf4e0336d38
Fajar Alif Riyandi <fajariyandi36@gmail.com>
2023-07-25 23:31:41 +0700

    Update PresenceGetId.php
    fixing message response

--- application/controllers/Absen/PresenceGetId.php
+++ application/controllers/Absen/PresenceGetId.php
@@ -83,7 +83,7 @@ class PresenceGetId extends RestController {
 					$this->response([
 						'status' => 404,
 						'error' => "true",
-						'message' => 'Maaf data absen pada tanggal ' . $date . ' di diklat ' .$judul. ' tidak ada',
+						'message' => 'Maaf data absen pada tanggal ' . $date . ' di ' .$judul. ' tidak ada',
 					], RestController::HTTP_BAD_REQUEST);
 				}
 			}
