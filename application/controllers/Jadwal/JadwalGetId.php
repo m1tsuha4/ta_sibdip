fb9a6dca1f9fcf7e05c5c628cd5c804aa9fc4988
Fajar Alif Riyandi <fajariyandi36@gmail.com>
2023-07-25 23:45:51 +0700

    Update JadwalGetId.php
    fixing message response & adding m_diklat

--- application/controllers/Jadwal/JadwalGetId.php
+++ application/controllers/Jadwal/JadwalGetId.php
@@ -10,42 +10,54 @@ class JadwalGetId extends RestController {
         // Construct the parent class
         parent::__construct();
         $this->load->model('m_jadwal');
+        $this->load->model('m_diklat');
     }
 
     //mendapatkan id
     public function JadwalGetById_get($cari = NULL)
     {
     $jadwal = new m_jadwal;
+    $diklat = new m_diklat;
     $result = $jadwal->GetByIdJadwal($cari);
 
-    //datanya 1
-    if (count($result) > 1) {
-        $this->response([
-            'status' => 200,
-            'error' => "false",
-            'message' => 'Jadwal Available',
-            'totaldata' => count($result),
-            'data' => $result
-        ], RestController::HTTP_OK);
-    }
-    //kalau nilainya lebih dari 1
-    elseif (count($result) === 1) {
-        $this->response([
-            'status' => 200,
-            'error' => "false",
-            'message' => 'Jadwal Available',
-            'totaldata' => count($result),
-            'data' => $result
-        ], RestController::HTTP_OK);
-    }
-    //kalau nilainya tidak ada
+    $diklatNames = $diklat->getDiklatName($cari);
+
+    if(!empty($diklatNames)){
+        //datanya 1
+        $judulDiklat = $diklatNames[0]['assessment_name'];
+        if (count($result) > 1) {
+            $this->response([
+                'status' => 200,
+                'error' => "false",
+                'message' => 'Jadwal Available',
+                'totaldata' => count($result),
+                'data' => $result
+            ], RestController::HTTP_OK);
+        }
+        //kalau nilainya lebih dari 1
+        elseif (count($result) === 1) {
+            $this->response([
+                'status' => 200,
+                'error' => "false",
+                'message' => 'Jadwal Available',
+                'totaldata' => count($result),
+                'data' => $result
+            ], RestController::HTTP_OK);
+        }else {
+            $this->response([
+                'status' => 404,
+                'error' => "true",
+                'message' => 'Maaf data jadwal/materi ' .$judulDiklat. ' tidak ada',  
+            ], RestController::HTTP_BAD_REQUEST);
+        }
+    }//kalau nilainya tidak ada
     else {
         $this->response([
             'status' => 404,
             'error' => "true",
             'message' => 'Maaf data jadwal/materi tidak ditemukan',
         ], RestController::HTTP_BAD_REQUEST);
-    }
+        }
     }
 
 }
